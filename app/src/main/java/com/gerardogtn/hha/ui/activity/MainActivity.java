afaa260452f0ef4015c70a0edae0d604257985f7
BraulioMendez <braulio9797@gmail.com>
2015-10-03 22:06:10 -0500

    :rocket: fill Recycler View with data alarm

--- app/src/main/java/com/gerardogtn/hha/ui/activity/MainActivity.java
+++ app/src/main/java/com/gerardogtn/hha/ui/activity/MainActivity.java
@@ -2,17 +2,43 @@ package com.gerardogtn.hha.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 import com.gerardogtn.hha.R;
+import com.gerardogtn.hha.ui.adapter.AlarmAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView newRecyclerView;
+    private RecyclerView.Adapter newAdapter;
+    private RecyclerView.LayoutManager newLayoutManager;
+
+    @Bind(R.id.list_alarms)
+    RecyclerView list_alarms;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newRecyclerView.setHasFixedSize(true);
+
+        newLayoutManager = new LinearLayoutManager(this);
+        newRecyclerView.setLayoutManager(newLayoutManager);
+
+        List<String> alarms = new ArrayList<>();
+        alarms.add("08:00");
+        alarms.add("09:00");
+        newAdapter = new AlarmAdapter(alarms);
+        newRecyclerView.setAdapter(newAdapter);
     }
 
     @Override

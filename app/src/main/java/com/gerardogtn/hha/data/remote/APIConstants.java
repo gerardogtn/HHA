44b67a827baacf221f4311e37d6bc319b51c9160
angelgladon <an098@hotmail.com>
2015-10-04 10:46:05 -0500

    Add APIConstants class

---
+++ app/src/main/java/com/gerardogtn/hha/data/remote/APIConstants.java
@@ -0,0 +1,16 @@
+package com.gerardogtn.hha.data.remote;
+
+/**
+ * Created by root on 4/10/15.
+ */
+public class APIConstants {
+
+    public static final String URL_BASE = "52.27.43.251";
+
+    public static final String PATH_HOST = "/host";
+    public static final String PATH_USERS = "/users";
+    public static final String PATH_DATE = "add_date";
+
+    public static final String ADD_DATE_POST = URL_BASE + PATH_HOST + PATH_USERS +PATH_DATE;
+
+}

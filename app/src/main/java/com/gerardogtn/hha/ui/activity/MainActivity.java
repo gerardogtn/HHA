ac093d6cc2873c7a40c9caaa929731b95eb7040f
Gerardo Teruel <gerardogtn@gmail.com>
2015-10-03 22:08:47 -0500

    :rocket: Update main activity

--- app/src/main/java/com/gerardogtn/hha/ui/activity/MainActivity.java
+++ app/src/main/java/com/gerardogtn/hha/ui/activity/MainActivity.java
@@ -1,19 +1,44 @@
 package com.gerardogtn.hha.ui.activity;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.gerardogtn.hha.R;
+import com.gerardogtn.hha.ui.adapter.PagerAdapter;
+import com.gerardogtn.hha.ui.fragment.AlarmFragment;
+import com.gerardogtn.hha.ui.fragment.ProfileFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Bind(R.id.tab_layout)
+    TabLayout mTabLayout;
+
+    @Bind(R.id.pager)
+    ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        setUpViewPager();
+        mTabLayout.setupWithViewPager(mViewPager);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,5 +55,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setUpViewPager() {
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(AlarmFragment.newInstance(), "Alarmas" );
+        adapter.addFragment(ProfileFragment.newInstance(), "Perfil" );
+        mViewPager.setAdapter(adapter);
     }
 }
